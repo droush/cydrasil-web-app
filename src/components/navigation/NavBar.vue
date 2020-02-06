95adac50016e1e5d98f7a709f7f68152879f3595
dwroush@asu.edu <dwroush@Friday.localdomain>
2020-02-05 17:43:05 -0700

    initial nav bar, button components, test drawer

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <v-app-bar
+      app
+      color="green"
+    >
+      <v-app-bar-nav-icon
+      color="white"
+      @click="drawer = !drawer" />
+
+      <div class="d-flex align-center">
+        <v-btn
+          to="/home"
+          text
+          color="white"
+        >
+          <span>Cydrasil</span>
+        </v-btn>
+
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <SignInButton/>
+
+    </v-app-bar>
+
+</template>
+
+<script>
+import SignInButton from '@/components/navigation/SignInButton'
+
+export default {
+
+  name: 'App',
+
+  components: {
+    SignInButton
+  }
+
+}
+</script>
---
+++ src/components/navigation/SignInButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-btn
+        to="/login"
+        target="_blank"
+        text
+        color="white"
+    >
+
+    <v-icon>mdi-account-circle-outline</v-icon>
+    <span class="ml-2">Log in</span>
+
+    </v-btn>
+</template>
